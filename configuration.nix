72890ee30f8a8a8d19efc2a558350a859ddbf446
Nixy <git.nixy.carefully356@passfwd.com>
2024-02-28 23:38:05 +0000

    Add main config

---
+++ configuration.nix
@@ -0,0 +1,61 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
+    ./disko.nix
+  ];
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "NixOS";
+  networking.networkmanager.enable = true;
+
+  # Locale
+  time.timeZone = "Europe/Warsaw";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "pl";
+  };
+  services.xserver.xkb.layout = "pl"; # X11
+
+  # Desktop
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Audio
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # Users
+  users.users = {
+    user = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      initialPassword = "qwe123";
+      packages = with pkgs; [
+        sl
+      ];
+    };
+  };
+
+  # Packages
+  environment.systemPackages = with pkgs; [
+    neofetch
+  ];
+
+  # https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion
+  system.stateVersion = "23.11";
+}
+
