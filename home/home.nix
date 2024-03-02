80429e4f808213a5dec1aaaca92fb59e1e4e2b01
NixyJuppie <git.nixy.carefully356@passfwd.com>
2024-03-02 04:47:47 +0100

    Add konsole config

--- home/home.nix
+++ home/home.nix
@@ -1,6 +1,6 @@
 { lib, pkgs, ... }:
 {
-  imports = [ ./desktop.nix ./web.nix ./dev.nix ./gaming.nix ];
+  imports = [ ./plasma.nix ./web.nix ./dev.nix ./gaming.nix ];
 
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
@@ -9,13 +9,14 @@
 
   home.username = "user";
   home.homeDirectory = "/home/user";
-  home.packages = with pkgs; [ beeper ];
+  home.packages = with pkgs; [ neofetch beeper ];
 
   programs.fish = {
     enable = true;
     interactiveShellInit = "set fish_greeting";
   };
   programs.starship.enable = true;
+  programs.nix-index.enable = true;
 
   programs.home-manager.enable = true;
   home.stateVersion = "23.11";
