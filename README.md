57e46414bef41857ee7bd1f010abd0274be61ba1
NixyJuppie <git.nixy.carefully356@passfwd.com>
2024-03-03 00:09:17 +0100

    Merge home-manager

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 <<<<<<< HEAD
 # NixOS Config
 
+## Installation
 > [!WARNING]  
 > These commands must be executed as root to avoid permission issues.  
 > Use `sudo su` to enter interactive root session instead of `sudo <command>`.
 
-## Partitioning
+### Partitioning
 ```bash
 # Set disk encryption password
 echo "PASSWORD" > /tmp/secret.key
@@ -14,7 +15,8 @@ git clone https://github.com/NixyJuppie/nixos /tmp/nixos
 # Format and partition disk (if you want to customize partition scheme, do it now)
 nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/nixos/disko.nix
 ```
-## Installation
+
+### Installation
 ```bash
 # Move config repo to /mnt/etc/nixos
 mkdir /mnt/etc && sudo mv /tmp/nixos /mnt/etc/nixos
@@ -27,20 +29,3 @@ nixos-install --flake /mnt/etc/nixos#nixos
 # Set user password
 nixos-enter -c "passwd user"
 ```
-
-## Home Manager
-Home Manager configuration and guide lives in separate [git repository](https://github.com/NixyJuppie/home-manager).
-=======
-# Home Manager Config
-
-## Installation
-> [!WARNING]  
-> These commands must be executed by an user named `user`.
-
-```bash
-# Clone this repo
-git clone https://github.com/NixyJuppie/home-manager ~/.config/home-manager
-# Build config
-nix run home-manager/master -- switch
-```
->>>>>>> home-manager/develop
--- flake.nix
+++ flake.nix
@@ -1,31 +1,12 @@
 {
-<<<<<<< HEAD
-  description = "NixOS Configuration";
+  description = "NixyJuppie NixOS Configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nurpkgs.url = "github:nix-community/NUR";
 
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = { nixpkgs, disko, ... }: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          # Disko
-          disko.nixosModules.disko
-          ./disko.nix
-
-          # System
-          ./configuration.nix
-=======
-  description = "Home Manager";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nurpkgs.url = "github:nix-community/NUR";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -35,19 +16,33 @@
     plasma-manager.inputs.home-manager.follows = "home-manager";
   };
 
-  outputs = { nixpkgs, nurpkgs, home-manager, plasma-manager, ... }: {
-    homeConfigurations = {
-      user = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [
-          plasma-manager.homeManagerModules.plasma-manager
-          ./home/home.nix
-          ({ pkgs, ... }: {
-            nixpkgs.overlays = [ nurpkgs.overlay ];
-          })
->>>>>>> home-manager/develop
-        ];
+  outputs = inputs @ { nixpkgs, nurpkgs, disko, home-manager, plasma-manager, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations = {
+        nixos = inputs.nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            disko.nixosModules.disko
+            ./disko.nix
+            ./configuration.nix
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        user = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.${system};
+          modules = [
+            plasma-manager.homeManagerModules.plasma-manager
+            ./home/home.nix
+            ({ pkgs, ... }: {
+              nixpkgs.overlays = [ nurpkgs.overlay ];
+            })
+          ];
+        };
       };
     };
-  };
 }
