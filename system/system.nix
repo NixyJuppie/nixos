e71a15c999eadfeb0db35815b82c5290d920d81b
NixyJuppie <git.nixy.carefully356@passfwd.com>
2024-03-03 15:24:22 +0100

    Fix vpn permissions

--- system/system.nix
+++ system/system.nix
@@ -11,32 +11,25 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Core
+  programs.git.enable = true;
+
+  # Drivers 
+  hardware.xpadneo.enable = true;
+
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Bluetooth
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-
-  # Xbox One Wireless Controller 
-  hardware.xpadneo.enable = true;
+  # Virtualisation
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
 
   # Networking
   networking.hostName = settings.hostname;
   networking.networkmanager.enable = true;
-
-  # Locale
-  time.timeZone = "Europe/Warsaw";
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "pl";
-  };
-
-  # Virtualisation
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
 
   # Desktop
   services.xserver.enable = true;
@@ -53,23 +46,31 @@
     jack.enable = true;
   };
 
+  # Locale
+  time.timeZone = "Europe/Warsaw";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "pl";
+  };
+
+  # Gaming
+  programs.steam.enable = true;
+  programs.gamemode.enable = true;
+
   # Packagess
   environment.systemPackages = with pkgs; [
     git
-    gamemode
     virtiofsd
   ];
 
   # Shells
   programs.fish.enable = true;
 
-  # Steam
-  programs.steam.enable = true;
-
   # Users
   users.users.${settings.username} = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "wheel" "networkmanager" ];
     shell = pkgs.fish;
   };
 
